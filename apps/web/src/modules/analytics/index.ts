11ad48a1c4528eb90e9fa6ba25bf4cb94a361c6f
MythRoll <djbrookman@googlemail.com>
2026-03-24 19:18:37 +0000

    Fix analytics promises

--- apps/web/src/modules/analytics/index.ts
+++ apps/web/src/modules/analytics/index.ts
@@ -1,2 +1,13 @@
-export * from "./api";
-export * from "./tracking";
+export async function trackPageView(
+  _page?: string,
+  _data?: Record<string, unknown>
+): Promise<void> {
+  return;
+}
+
+export async function trackEvent(
+  _event?: string,
+  _data?: Record<string, unknown>
+): Promise<void> {
+  return;
+}
